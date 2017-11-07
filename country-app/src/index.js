4863fc4fd459310ee8f84b328e23e018ba1ea02d
Tomasz Konecki <tomasz.konecki.75@gmail.com>
2017-11-07 16:27:45 +0100

    Update code

--- country-app/src/index.js
+++ country-app/src/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './store';
 
 render(
-    <h1>Inicjalizacja projektu</h1>,
+    <Provider store={store}>
+        <h1>Inicjalizacja projektu</h1>
+    </Provider>,
     document.getElementById('root')
 );
